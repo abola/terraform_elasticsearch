4d9ec89aa6c5b8199bc988dc5da4911a661c9a7e
Abola Lee <abola921@gmail.com>
2019-01-25 01:53:20 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,10 @@
 #####################################################################
-# Variables
-#####################################################################
-variable "username" {
-  default = "admin"
-}
-variable "password" {}
-variable "project" {}
-variable "region" {}
-#####################################################################
 # Modules
 #####################################################################
 module "elasticsearch" {
-  source = "./elasticsearch"
-  project = "${var.project}"
- region = "${var.region}"
+  source   = "./elasticsearch"
+  project  = "${var.project}"
+  region   = "${var.region}"
   username = "${var.username}"
   password = "${var.password}"
 }
