7982cb01ee6a1345aaa3db17fa855073f82481c6
Abola Lee <abola921@gmail.com>
2019-01-25 01:55:35 +0800

    Update elasticsearch.tf

--- elasticsearch/elasticsearch.tf
+++ elasticsearch/elasticsearch.tf
@@ -20,17 +20,4 @@ service_account {
 scopes = ["userinfo-email", "compute-ro", "storage-ro"]
 }
 
-metadata_startup_script = <<SCRIPT
-yum update -y
-
-yum install -y docker
-
-yum install docker-ce
-systemctl enable docker
-systemctl start docker
-docker pull docker pull docker.elastic.co/elasticsearch/elasticsearch:6.5.4
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.5.4
-
-SCRIPT
-
 }
