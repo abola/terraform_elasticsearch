d45cc359cd8f617bde75362ed6efd6213024b17c
Abola Lee <abola921@gmail.com>
2019-01-25 02:10:16 +0800

    Update elasticsearch.tf

--- elasticsearch/elasticsearch.tf
+++ elasticsearch/elasticsearch.tf
@@ -21,11 +21,11 @@ scopes = ["userinfo-email", "compute-ro", "storage-ro"]
 }
 metadata_startup_script = <<SCRIPT
 yum update -y
- yum install -y docker
- yum install docker-ce
+yum install -y docker
+yum install -y docker-ce
 systemctl enable docker
 systemctl start docker
 docker pull docker pull docker.elastic.co/elasticsearch/elasticsearch:6.5.4
 docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.5.4
- SCRIPT
+SCRIPT
 }
